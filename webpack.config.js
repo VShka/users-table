511c29ed43e96dadebd3ba564f2b5d8b5de95692
VShka <viktor.shkarupa@yandex.ru>
2020-10-02 16:06:19 +0300

    delete hash for hot-reload. 2) Add dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const WebpackMd5Hash = require("webpack-md5-hash");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const isDev = process.env.NODE_ENV === "development";
 
@@ -12,7 +11,7 @@ module.exports = {
     main: "./app/index.js"
   },
   output: {
-    filename: "[name].[chunkhash].js",
+    filename: "index.js",
     path: path.resolve(__dirname, "dist")
   },
   module: {
@@ -25,10 +24,7 @@ module.exports = {
         test: /\.scss$/i,
         use: [
           isDev ? 'style-loader' : {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              publicPath: "../"
-            }
+            loader: MiniCssExtractPlugin.loader
           },
           'css-loader',
           'postcss-loader',
@@ -39,7 +35,7 @@ module.exports = {
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: "[name].[contenthash].css"
+      filename: "style.css"
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
@@ -52,10 +48,8 @@ module.exports = {
     new HtmlWebpackPlugin({
       inject: false,
       template: "./public/index.pug",
-      filename: "index.html",
-      chunks: ["main"]
+      filename: "index.html"
     }),
-    new WebpackMd5Hash(),
     new webpack.DefinePlugin({
       "NODE_ENV": JSON.stringify(process.env.NODE_ENV)
     })
