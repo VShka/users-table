ca6582881fc83032059ce4b36a24ffaf3953f04f
VShka <viktor.shkarupa@yandex.ru>
2020-10-06 16:54:02 +0300

    delete btn cancel. rebuild styles

--- src/app/index.js
+++ src/app/index.js
@@ -9,6 +9,8 @@ const openAddUser = $('.button_add');
 const openDeleteUser = $('.button_delete');
 const openEditUser = $('.button_edit');
 const closeModal = $('.button_close');
+const addBtn = $('.form__item_add');
+const confirmBtn = $('.button_confirm');
 //модалки
 const addModal = $('#modal-form');
 const confirmModal = $('#modal-confirm');
