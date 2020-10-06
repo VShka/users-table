a76c0d847b9acfa5ebdb85c2e2713295e9dfaf19
VShka <viktor.shkarupa@yandex.ru>
2020-10-06 17:52:50 +0300

    Add method create, which create template for user

--- src/app/components/User.js
+++ src/app/components/User.js
@@ -1,7 +1,29 @@
+import $ from 'jquery';
+
 export default class User {
   constructor() {
 
   }
 
-  
+
+  create(props) {
+    
+    // html разметка на pug
+    const template = $(`
+      .user-table__row
+        p.user-table__item ${props.name}
+        p.user-table__item ${props.birthday}
+        p.user-table__item ${props.placeOfBirth}
+        p.user-table__item ${props.email}
+        p.user-table__item ${props.phone}
+        p.user-table__item ${props.dateReg}
+        p.user-table__item ${props.dateLastVisit}
+        p.user-table__item
+          button.button.button_edit Редактировать
+        p.user-table__item
+          button.button.button_delete Удалить
+    `)
+
+    return template;
+  }
 }
